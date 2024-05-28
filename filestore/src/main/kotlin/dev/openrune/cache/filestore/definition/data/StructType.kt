92ed70b01ae57a3c395a763d6c159e629707bbd7
MatthewBishop <55411296+MatthewBishop@users.noreply.github.com>
2024-05-27 23:06:50 -0400

    fix: struct decoding

--- filestore/src/main/kotlin/dev/openrune/cache/filestore/definition/data/StructType.kt
+++ filestore/src/main/kotlin/dev/openrune/cache/filestore/definition/data/StructType.kt
@@ -1,15 +1,14 @@
 package dev.openrune.cache.filestore.definition.data
 
 import dev.openrune.cache.filestore.definition.Definition
-import dev.openrune.cache.filestore.definition.Extra
+import dev.openrune.cache.filestore.definition.Parameterized
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap
 
 data class StructType(
     override var id: Int = -1,
-    override var stringId: String = "",
-    override var extras: Map<String, Any>? = null,
-
-    override var inherit: Int = -1
-) : Definition, Extra {
+    override var inherit: Int = -1,
+    override var params: Map<Int, Any>? = Int2ObjectOpenHashMap()
+) : Definition, Parameterized {
     companion object {
         val EMPTY = StructType()
     }
