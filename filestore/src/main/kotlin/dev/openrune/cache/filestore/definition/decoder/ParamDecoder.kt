f57168de7b00a34f2232eb67daa9b72f73b7ae09
MatthewBishop <55411296+MatthewBishop@users.noreply.github.com>
2024-09-09 19:26:50 -0400

    add: param encoding and decoding

---
+++ filestore/src/main/kotlin/dev/openrune/cache/filestore/definition/decoder/ParamDecoder.kt
@@ -0,0 +1,29 @@
+package dev.openrune.cache.filestore.definition.decoder
+
+import dev.openrune.cache.PARAMS
+import dev.openrune.cache.STRUCT
+import dev.openrune.cache.filestore.definition.DefinitionDecoder
+import dev.openrune.cache.filestore.buffer.Reader
+import dev.openrune.cache.filestore.definition.data.ParamType
+import dev.openrune.cache.filestore.definition.data.StructType
+import dev.openrune.cache.util.ScriptVarType
+
+class ParamDecoder : DefinitionDecoder<ParamType>(PARAMS) {
+
+    override fun create(size: Int) = Array(size) { ParamType(it) }
+
+    override fun getFile(id: Int) = id
+
+    override fun ParamType.read(opcode: Int, buffer: Reader) {
+        when (opcode) {
+            1 -> {
+                val idx = buffer.readUnsignedByte()
+                type = ScriptVarType.forCharKey(idx.toChar())
+            }
+
+            2 -> defaultInt = buffer.readInt()
+            4 -> isMembers = false
+            5 -> defaultString = buffer.readString()
+        }
+    }
+}
