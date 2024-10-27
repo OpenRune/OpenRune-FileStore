fa2419e23c63199807206755a3fa42cdb60ed0f0
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2024-10-27 11:49:43 +0000

    226

--- filestore/src/main/kotlin/dev/openrune/cache/CacheManager.kt
+++ filestore/src/main/kotlin/dev/openrune/cache/CacheManager.kt
@@ -8,7 +8,7 @@ import java.nio.file.Path
 object CacheManager {
 
     lateinit var cache: Cache
-    private var cacheRevision = -1
+    var cacheRevision = -1
 
     private val npcs = mutableMapOf<Int, NpcType>()
     private val objects = mutableMapOf<Int, ObjectType>()
