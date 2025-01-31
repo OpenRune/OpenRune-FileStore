aad99028ea6c0c67b2b757495fb124b3dbdbe5b0
MatthewBishop <55411296+MatthewBishop@users.noreply.github.com>
2025-01-31 01:20:17 -0500

    remove: opcodeless decoder
    this is redundant and useless now.

--- cache/osrs/src/main/kotlin/dev/openrune/decoder/SpriteDecoder.kt
+++ cache/osrs/src/main/kotlin/dev/openrune/decoder/SpriteDecoder.kt
@@ -1,10 +1,10 @@
 package dev.openrune.decoder
 
-import dev.openrune.OpcodelessDecoder
 import dev.openrune.cache.SPRITES
+import dev.openrune.cache.filestore.definition.DefinitionDecoder
 import dev.openrune.cache.filestore.definition.data.SpriteType
 import dev.openrune.codec.SpriteCodec
 
-class SpriteDecoder : OpcodelessDecoder<SpriteType>(SPRITES, SpriteCodec()) {
+class SpriteDecoder : DefinitionDecoder<SpriteType>(SPRITES, SpriteCodec()) {
     override fun getFile(id: Int) = 0
 }
--- cache/osrs/src/main/kotlin/dev/openrune/decoder/TextureDecoder.kt
+++ cache/osrs/src/main/kotlin/dev/openrune/decoder/TextureDecoder.kt
@@ -1,10 +1,11 @@
 package dev.openrune.decoder
 
-import dev.openrune.OpcodelessDecoder
 import dev.openrune.cache.TEXTURES
+import dev.openrune.cache.filestore.definition.DefinitionDecoder
 import dev.openrune.cache.filestore.definition.data.TextureType
 import dev.openrune.codec.TextureCodec
 
-class TextureDecoder : OpcodelessDecoder<TextureType>(TEXTURES, TextureCodec()) {
+class TextureDecoder : DefinitionDecoder<TextureType>(TEXTURES, TextureCodec()) {
     override fun getArchive(id: Int) = 0
+    override fun getFile(id: Int) = 0
 }
