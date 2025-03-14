a87180436d467e4b15d265da44b63488e8c1d639
MatthewBishop <55411296+MatthewBishop@users.noreply.github.com>
2025-03-13 21:45:03 -0400

    add: displee sources from 5b8733d

---
+++ displee/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    implementation("com.github.jponge:lzma-java:1.3")
+    implementation("org.apache.ant:ant:1.10.14")
+    implementation("com.displee:disio:2.2")
+}
--- tools/build.gradle.kts
+++ tools/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     implementation("me.tongfei:progressbar:0.9.2")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("commons-io:commons-io:2.15.1")
-    implementation("com.displee:rs-cache-library:7.1.5")
+    implementation(project(":displee"))
     implementation("com.akuleshov7:ktoml-core:0.5.1")
     implementation("com.akuleshov7:ktoml-file:0.5.1")
 }
