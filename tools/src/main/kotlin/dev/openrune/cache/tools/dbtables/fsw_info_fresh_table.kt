5e5682fe12f025a0f7a3fbaf766fd0dc06b80105
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2025-04-29 22:57:02 +0100

    g

--- tools/src/main/kotlin/dev/openrune/cache/tools/dbtables/fsw_info_fresh_table.kt
+++
@@ -1,8 +0,0 @@
-package dev.openrune.cache.tools.dbtables
-
-class fsw_info_fresh_table(row: FullDBRow) : DatabaseRow(row) {
-    val info = multiColumn(0, StringType, StringType, StringType)
-    override fun toString(): String {
-        return "fsw_info_fresh_table(column0=$info)"
-    }
-}
