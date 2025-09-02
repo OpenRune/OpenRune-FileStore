92f8fd0bdcc0f4dee018e20a5a31145de1a51b63
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2025-09-02 01:56:26 +0100

    Update ConfigTest.kt

--- tools/src/test/kotlin/dev/openrune/ConfigTest.kt
+++ tools/src/test/kotlin/dev/openrune/ConfigTest.kt
@@ -1,10 +1,13 @@
 package dev.openrune
 
 import dev.openrune.cache.CacheManager
+import dev.openrune.cache.gameval.GameValHandler
+import dev.openrune.cache.gameval.GameValHandler.lookup
 import dev.openrune.cache.tools.Builder
 import dev.openrune.cache.tools.tasks.CacheTask
 import dev.openrune.cache.tools.tasks.TaskType
 import dev.openrune.cache.tools.tasks.impl.defs.PackConfig
+import dev.openrune.definition.GameValGroupTypes
 import dev.openrune.filesystem.Cache
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -16,7 +19,8 @@ class ConfigTest {
 
 
     companion object {
-        val REV = 225
+        val REV = 232
+        lateinit var cache : Cache
         val cacheLocation = File("${System.getProperty("java.io.tmpdir")}/openrune/$REV")
 
         @BeforeAll
@@ -37,10 +41,11 @@ class ConfigTest {
                     PackConfig(resourceFile)
                 ).toMutableList()
 
-                val builder = Builder(type = TaskType.BUILD, revision = REV, cacheLoc)
+                val builder = Builder(type = TaskType.BUILD, cacheLoc).revision(REV)
                 builder.registerRSCM(File(ConfigTest::class.java.classLoader.getResource("mappings")!!.toURI()))
                 builder.extraTasks(*tasks.toTypedArray()).build().initialize()
-                CacheManager.init(OsrsCacheProvider(Cache.load(cacheLoc.toPath(), false), REV))
+                cache = Cache.load(cacheLoc.toPath(), false)
+                CacheManager.init(OsrsCacheProvider(cache))
             }
         }
     }
@@ -51,12 +56,15 @@ class ConfigTest {
         val objectId = 60005
         val objectDef = CacheManager.getObjectOrDefault(objectId)
 
+        val objVals = GameValHandler.readGameVal(GameValGroupTypes.LOCTYPES,cache)
+
         assertEquals("Merlin Rune Portal",objectDef.name)
         assertEquals(2212,objectDef.animationId)
         assertEquals(2,objectDef.sizeX)
         assertEquals(2,objectDef.sizeY)
         assertEquals(listOf("Teleport", null, null, null, null),objectDef.actions)
         assertEquals(listOf(63101), objectDef.objectModels)
+        assertEquals("merlin_rune_portal", objVals.lookup(objectDef.id)?.name)
     }
 
     @Test
