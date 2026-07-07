2d3c6fca38ed5f0c88985ad3959eb1cc86917374
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2026-07-07 13:25:33 +0100

    239

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 //val buildDirectory = "/home/advo/Documents/GitHub/hosting/"
 val buildDirectory = System.getenv("HOSTING_DIRECTORY") ?: "D:\\OpenRune\\openrune-hosting"
-val buildNumber = "2.4.12"
+val buildNumber = "2.4.13"
 
 subprojects {
     apply(plugin = "kotlin")
